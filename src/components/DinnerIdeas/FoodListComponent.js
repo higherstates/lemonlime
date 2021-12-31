51a513209dce359f6de02aa95ef5afb96604c59e
higherstates <thanhthuyh2007@gmail.com>
2021-12-31 11:42:43 +0700

    Edit CSS on @media

--- src/components/DinnerIdeas/FoodListComponent.js
+++ src/components/DinnerIdeas/FoodListComponent.js
@@ -7,36 +7,36 @@ export default function FoodListComponent(props) {
                 Food List
             </h1>
             <table className="flist__table">
-            <thead>
-                <tr>
-                    <th className="flist__table--name">
-                        Food
-                        <span 
-                            aria-label="Please only edit/delete your own inserted ideas" data-balloon-pos="up"
-                            className="i__btn tooltip-green  tooltip-big-text"
-                        >
-                            !
-                        </span>
-                    </th>
-                    <th className="flist__table--price">Price</th>
-                    <th className="flist__table--actions">
-                        Action
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.foodList.length > 0 && props.foodList.map((val, key) => {
-                    return (
-                        <FoodListTable
-                            key={key} 
-                            val={val}
-                            deleteFood={props.deleteFood}
-                            updateFood={props.updateFood}
-                            foodUrl={props.foodUrl}
-                        />    
-                    )
-                })}
-            </tbody>
+                <thead>
+                    <tr>
+                        <th className="flist__table--name">
+                            Food
+                            <span 
+                                aria-label="Please only edit/delete your own inserted ideas" data-balloon-pos="up"
+                                className="i__btn tooltip-green  tooltip-big-text"
+                            >
+                                !
+                            </span>
+                        </th>
+                        <th className="flist__table--price">Price</th>
+                        <th className="flist__table--actions">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.foodList.length > 0 && props.foodList.map((val, key) => {
+                        return (
+                            <FoodListTable
+                                key={key} 
+                                val={val}
+                                deleteFood={props.deleteFood}
+                                updateFood={props.updateFood}
+                                foodUrl={props.foodUrl}
+                            />    
+                        )
+                    })}
+                </tbody>
             </table>
         </div>
     )
